ccedb04a858843717e43e0638548bb0439d68c62
Bahador <49520007+Baha2rM98@users.noreply.github.com>
2019-07-16 01:24:06 +0430

    UI improved

--- src/RSAMain.java
+++ src/RSAMain.java
@@ -17,14 +17,15 @@ public class RSAMain {
                 System.out.println("Enter Text : (enter q or Q for exit)");
                 String s = scn.nextLine();
                 if (s.equals("q") || s.equals("Q")) {
-                    System.out.println("terminated!");
+                    System.out.println("Terminated!");
                     break;
+                } else {
+                    ArrayList<String> e = rsa.encryption(s);
+                    System.out.println(e);
+                    String d = rsa.decryption(e);
+                    System.out.println(d);
+                    System.out.println();
                 }
-                ArrayList<String> e = rsa.encryption(s);
-                System.out.println(e);
-                String d = rsa.decryption(e);
-                System.out.println(d);
-                System.out.println();
             }
         }
         if (ans.equals("2")) {
@@ -32,14 +33,22 @@ public class RSAMain {
                 System.out.println("Enter the path of your file's directory (for example C:\\User\\...)\nEnter q or Q for exit");
                 String way = scn.next();
                 if (way.equals("q") || way.equals("Q")) {
-                    System.out.println("terminated!");
+                    System.out.println("Terminated!");
                     break;
+                } else {
+                    File path = new File(way);
+                    System.out.println("Enter your file name with suffix (for example .txt .bin and...)");
+                    String name = scn.next();
+                    rsa.fileEncryption(path, name);
+                    System.out.println("Do you wanna decrypt this file?\nPress y or n...");
+                    String res = scn.next();
+                    if (res.equals("y") || res.equals("Y")) {
+                        rsa.fileDecryption(path, name);
+                    } else {
+                        System.out.println("Ops! you haven't chosen y!\nTerminated...");
+                        break;
+                    }
                 }
-                File path = new File(way);
-                System.out.println("Enter your file name with suffix (for example .txt .bin and...)");
-                String name = scn.next();
-                rsa.fileEncryption(path, name);
-                rsa.fileDecryption(path, name);
             }
         }
     }
